830b66f6b2190220a4a0b3d1a993408e9d512ab9
Humdiess <hmzincannel@gmail.com>
2024-05-02 14:45:46 +0700

    feat: add mashud

---
+++ resources/views/mashud.blade.php
@@ -0,0 +1,1 @@
+<P>hudi kaya anjing</P>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hudi', function () {
+    return view('mashud');
+});
+
+
