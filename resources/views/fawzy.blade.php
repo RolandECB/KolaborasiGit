4f07cd5caece80656d435d30ba7c260b14ab395b
Khulafaur Rasyidin <cyberrashad@gmail.com>
2024-05-02 14:46:55 +0700

    feat: nameFile blade template

---
+++ resources/views/fawzy.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fawzy - Code</title>
+</head>
+<body>
+    <h1>ini halaman saya</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima unde doloremque voluptatibus quibusdam aperiam velit perferendis ad ipsa repellendus, earum optio modi amet aliquam sit magnam. Iure error doloribus enim.</p>
+</body>
+</html>
